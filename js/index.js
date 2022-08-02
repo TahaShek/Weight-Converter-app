bcbd660f0758d34b091235376d452ab0d806ebc9
TahaSekk <sohailtaha128@gmail.com>
2022-08-02 05:27:59 +0500

    commti

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// Sab se phela sare variable declare kro 
+
+let output = document.querySelector(".output");
+output.style.visibility = "hidden";
+let EnterWeight = document.querySelector("#weightInput");
+let grams = document.querySelector("#gramsOutput");
+let pound = document.querySelector("#poundOutput");
+let Ounces = document.querySelector("#ozOutput");
+
+// function jo input field par lag rha ha  
+// target madddad krta ha specific cheex hit krna mae 
+// uska baad basic inner html mare formula use kro jo convert krna ka ha 
+
+EnterWeight.addEventListener("input", function (e) {
+  output.style.visibility = "visible";
+  let kg = e.target.value;
+  grams.innerHTML = kg * 1000;
+  pound.innerHTML = kg * 2.20462;
+  Ounces.innerHTML = kg * 35.274;
+});
